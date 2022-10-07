29ca1d4b39127efb869960bc849b0cf84a279d68
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-07 15:15:18 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchemployeeComponent } from './searchemployee/searchemployee.component';
+import { Route, RouterModule, Routes } from '@angular/router';
 
+
+const appRoutes: Routes = [
+  {
+    path:"", component:AddemployeeComponent
+  },
+  {
+    path:"search", component:SearchemployeeComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +26,8 @@ import { SearchemployeeComponent } from './searchemployee/searchemployee.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
