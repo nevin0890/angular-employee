4780a2322a552fc96922fae413bf20b3172b6def
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-07 13:47:45 +0530

    add employee created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddemployeeComponent
   ],
   imports: [
     BrowserModule,
